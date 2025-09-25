748c3fd96566001533658ee67978d9f4168a8013
Irina Sirenko <isirenko@samokat.ru>
2025-09-25 08:04:50 +0300

    Повторение автотеста из урока 2. Тест "Попытка обновить несуществующего питомца"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,7 +1,7 @@
 import allure
 import requests
 BASE_URL = "http://5.181.109.28:9090/api/v3" #путь из сваггера, основной адрес, а далее идет ендпоинт
-
+#урок 1
 @allure.feature("Pet")
 class TestPet:
     @allure.title("Попытка удалить несуществующего питомца")
@@ -12,3 +12,16 @@ class TestPet:
             assert response.status_code == 200, "Код ответа не совпадает с ожидаемым"
         with allure.step("Проверка содержимого body ответа"):
             assert response.text == 'Pet deleted', "Текст ошибки не совпадает с ожидаемым"
+#урок 2
+    @allure.title("Попытка обновить несуществующего питомца")
+    def test_update_nonexistent_pet(self):
+        with allure.step("Отправка запроса на удаление несуществующего питомца"):
+            payload = {"id": 9999,
+                       "name": "Non-existent Pet",
+                       "status": "available"
+                       }
+            response = requests.put(url=f'{BASE_URL}/pet', json=payload)
+        with allure.step("Проверка статус-кода ответа"):
+            assert response.status_code == 404, "Код ответа не совпадает с ожидаемым"
+        with allure.step("Проверка текста ответа"):
+            assert response.text == "Pet not found", "Текст ошибки не совпадает с ожидаемым"
